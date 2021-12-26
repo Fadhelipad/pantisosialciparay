769245cced838ea495fcf806e4a6cc943a3a4c40
gumilar.pratama <gumilar.pratama@kinnarastudio.com>
2021-12-26 20:50:38 +0700

    add google maps

--- lib/Petugas/maps_petugas.dart
+++ lib/Petugas/maps_petugas.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
 
 class Mapspetugas extends StatefulWidget {
   @override
@@ -7,18 +12,42 @@ class Mapspetugas extends StatefulWidget {
     // TODO: implement createState
     return MapsPetugasState();
   }
-  }
-  class MapsPetugasState extends State<Mapspetugas> {
+}
 
-    @override
-    Widget build(BuildContext context) {
-      // TODO: implement bui
-      return Scaffold(
-          appBar: AppBar(
-          title: Text("Maps Petugas"),
-      backgroundColor: Colors.blue[200],
-      )
-      );
-  }
+class MapsPetugasState extends State<Mapspetugas> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
   }
 
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
